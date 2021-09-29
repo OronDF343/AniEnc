f8034145f5b1216d5c99209157be919b0d9ab5ff
OronDF343 <orondf343@gmail.com>
2021-09-29 23:09:09 +0300

    Add GIF support

--- AniEnc/Program.cs
+++ AniEnc/Program.cs
@@ -4,6 +4,7 @@ using System.IO.Compression;
 using System.Text.Json;
 using System.Threading.Tasks;
 using ImageMagick;
+using ImageMagick.Formats;
 
 namespace AniEnc
 {
@@ -11,13 +12,13 @@ namespace AniEnc
     {
         static async Task Main(string[] args)
         {
-            if (args.Length < 3)
+            if (args.Length < 1)
             {
                 PrintUsage();
                 return;
             }
             
-            var fileName = args[2];
+            var fileName = args[0];
             string zipFile;
             string jsFile;
             var ext = Path.GetExtension(fileName);
@@ -25,18 +26,38 @@ namespace AniEnc
             {
                 zipFile = fileName;
                 jsFile = zipFile + ".js";
+
+                await ConvertUgoira(zipFile, jsFile);
             }
             else if (string.Equals(ext, ".js", StringComparison.OrdinalIgnoreCase))
             {
                 jsFile = fileName;
                 zipFile = jsFile[..^3];
+
+                await ConvertUgoira(zipFile, jsFile);
+            }
+            else if (string.Equals(ext, ".jxl", StringComparison.OrdinalIgnoreCase))
+            {
+                using var infoImg = new MagickImageCollection(args[0], MagickFormat.Jxl);
+                Console.WriteLine($"Count:{infoImg.Count}");
+                Console.WriteLine($"Size0:{infoImg[0].Width}x{infoImg[0].Height}");
+                Console.WriteLine($"Delay0:{infoImg[0].AnimationDelay}");
+            }
+            else if (string.Equals(ext, ".gif", StringComparison.OrdinalIgnoreCase))
+            {
+                using var inImg = new MagickImageCollection(fileName);
+                inImg.Flatten();
+                var outFilePath = Path.ChangeExtension(fileName, "jxl");
+                await inImg.WriteAsync(outFilePath, MagickFormat.Jxl);
             }
             else
             {
                 Console.Error.WriteLine("Invalid input file extension");
-                return;
             }
+        }
 
+        private static async Task ConvertUgoira(string zipFile, string jsFile)
+        {
             if (!File.Exists(zipFile))
             {
                 Console.Error.WriteLine($"File not found: {zipFile}");
@@ -74,14 +95,14 @@ namespace AniEnc
 
             await using var outFile = new FileStream(outFilePath, FileMode.Create, FileAccess.ReadWrite, FileShare.None,
                                                      4096, FileOptions.Asynchronous);
-            await outImg.WriteAsync(outFile, MagickFormat.Jxl);
+            await outImg.WriteAsync(outFile, new JxlWriteDefines { Effort = 6 });
         }
 
         private static void PrintUsage()
         {
             Console.Error.WriteLine("AniEnc (c) 2021 Oron Feinerman");
-            Console.Error.WriteLine("Usage: anienc <parameters> [infile] <outfile>");
-            Console.Error.WriteLine("Implemented: anienc -i:uzj -o:jxl infile.zip");
+            Console.Error.WriteLine("Usage: anienc [infile] <outfile>");
+            Console.Error.WriteLine("Implemented: anienc infile.zip");
         }
     }
 }
